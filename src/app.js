6ac72b0c040670d5f6552c1cbc1b4e834406a684
Sujeet17m <sujeet17das@gmail.com>
2025-06-26 09:21:15 +0530

    now you have to restart server after every minor change so, need to install nodemon

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res)=> {
+app.use("/test",(req,res)=> {
     res.send("hello from the server");
 });
 
+app.use("/hello",(req,res)=> {
+    res.send("hello heloo helllo");
+});
 app.listen(3001 , () => {
     console.log("server is listening");
     
