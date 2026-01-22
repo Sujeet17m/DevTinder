16ca8cdd955f29ca25fdf5b5e96d32eadd7ac7c0
Sujeet17m <sujeet17das@gmail.com>
2026-01-22 23:57:55 +0530

    Fix comment formatting in authRouter.js

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const authRouter = express.Router();
 
-// Example route for user login
+//  Example route for user login
 authRouter.post('/login' ,usetAuth, (req, res) => {
     res.send("User login endpoint");
 
